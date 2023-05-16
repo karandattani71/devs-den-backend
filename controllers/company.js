545e8d02b6635cc19269d4a96ca1c0ce94913007
karandattani7 <118350705+karandattani7@users.noreply.github.com>
2023-05-16 12:05:12 +0530

    added devs den backend code

---
+++ controllers/company.js
@@ -0,0 +1,125 @@
+const Company = require("../models/company");
+const Post = require("../models/post");
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+
+exports.getCompanyById = (req, res, next, id) => {
+       Company.findById(id)
+              .populate("followers","_id name avatar")
+              .populate("posts","_id description imageUrl comments likes")
+              .exec((err, company) => {
+              if(err)
+                     return res.status(400).json({error:err})
+              else if(!company)
+                     return res.status(400).json({error:"Company not found!"})
+              else {
+                     req.profile = company;
+                     next();
+              }
+       });
+}
+
+exports.getCompany = (req,res) => {
+       req.profile.salt = undefined;
+       req.profile.encry_password = undefined;
+       req.profile.createdAt = undefined;
+       req.profile.updatedAt = undefined;
+       return res.json(req.profile);
+}
+
+exports.getAllCompanies = (req, res) => {
+       Company.find({})
+       .select("name field avatar followers")
+       .exec((err, companies) => {
+              if(err)
+                     return res.status(400).json({error:"No companies found"});
+              return res.json(companies);
+       });
+}
+
+exports.getCompanyBySpeciality = (req, res) => {
+       Company.find({specialities: { $elemMatch: {'$regex': req.body.speciality, $options:'i'} }})
+       .select("name avatar size followers")
+       .exec((err, companies) => {
+              if(err)
+                     return res.status(400).json({error:"No company found with this speciality"});
+              return res.json(companies);
+       });
+}
+
+exports.getCompanyByName = (req, res) => {
+       Company.find({name: {'$regex': req.body.name, $options:'i'} })
+       .select("name avatar size followers")
+       .exec((err, companies) => {
+              if(err)
+                     return res.status(400).json({error:"No company found with this name"});
+              return res.json(companies);
+       });
+}
+
+exports.getCompanyFollowers = (req, res) => {
+       return res.json(req.profile.followers)
+}
+
+exports.updateCompany = (req, res) => {
+       Company.findByIdAndUpdate(
+              {_id: req.profile._id},
+              {$set: req.body},
+              {new: true, useFindAndModify: false},
+              (err, company) => {
+                   if(err)
+                        return res.status(400).json({error:"You are not authorized to update the info!"});
+                     company.salt = undefined;
+                     company.encry_password = undefined;
+                     company.createdAt = undefined;
+                     company.updatedAt = undefined;
+                     res.json(company);
+         })
+}
+
+exports.deleteCompany = (req, res) => {
+       const company = req.profile;
+       var error;
+
+       company.posts.map((post) => {
+              Post.findById(post._id)
+                     .exec((err, p) => {
+                            if(err)
+                                   error = {error:"Failed to delete the post!"};
+                            p.remove((err, pst) => {
+                                   if(err)
+                                          error =  {error:"Failed to remove the post!"}
+                            })
+                     })
+       })
+
+       company.remove((err, company) => {
+              if(err)
+                     return res.status(400).json({error:"Failed to delete the account. "+error});
+              res.json({message:"Successfully deleted "+company.name+"'s account."});
+       });
+}
+
+exports.verifyCompany = (req, res) => {
+       if(!req.profile.isVerified) {
+              if(req.params.token==req.profile.token) {
+                     Company.findByIdAndUpdate(
+                            {_id: req.profile._id},
+                            {isVerified: true},
+                            {new: true},
+                            (err, company) => {
+                            if(err)
+                                   return res.status(400).json({error:"Failed to add user to your following list!"});
+                            const authtoken = jwt.sign({ _id: company._id }, process.env.SECRET);
+                            res.cookie("token", authtoken, { expiresIn: 60 * 60 * 9999});
+                                   
+                            const {_id, name, email, role, type, avatar, isVerified} = company;
+                            return res.json({authtoken,user: {_id, name, email, role, type, avatar, isVerified}});
+                     })
+              } else {
+                     return res.status(400).json({error:"Invalid verification link!"});
+              }
+       } else {
+              return res.status(400).json({error:"This account is already verified!"});
+       }
+}
---
+++ routes/company.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const {getCompanyById, getCompany, getAllCompanies, getCompanyBySpeciality, getCompanyByName, verifyCompany, getCompanyFollowers, updateCompany, deleteCompany} = require("../controllers/company");
+const {isSignedIn, isAuthenticated} = require("../controllers/auth");
+
+router.param("companyId", getCompanyById);
+
+//read
+router.get("/company/:companyId", isSignedIn, getCompany);
+router.get("/companies/all", isSignedIn, getAllCompanies);
+router.get("/company/:companyId/followers", isSignedIn, getCompanyFollowers);
+
+router.post("/company/search/speciality", getCompanyBySpeciality);
+router.post("/company/search/name", getCompanyByName);
+
+router.get("/company/verify/:companyId/:token", verifyCompany);
+
+//update
+router.put("/company/:companyId/update", isSignedIn, isAuthenticated, updateCompany);
+
+//delete
+router.delete("/company/:companyId/delete", isSignedIn, isAuthenticated, deleteCompany);
+
+module.exports = router;
